198aa1558f9cf041e99bcce95c1b52f860fa381c
Mesut GENEZ <genezmesut@gmail.com>
2025-03-08 23:48:48 +0300

    logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,90 @@
+package logger
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+// LogLevel represents the level of logging
+type LogLevel int
+
+const (
+	// DEBUG level
+	DEBUG LogLevel = iota
+	// INFO level
+	INFO
+	// WARN level
+	WARN
+	// ERROR level
+	ERROR
+	// FATAL level
+	FATAL
+)
+
+var levelNames = map[LogLevel]string{
+	DEBUG: "DEBUG",
+	INFO:  "INFO",
+	WARN:  "WARN",
+	ERROR: "ERROR",
+	FATAL: "FATAL",
+}
+
+// Logger represents a logger
+type Logger struct {
+	level  LogLevel
+	logger *log.Logger
+}
+
+// New creates a new logger
+func New(level LogLevel) *Logger {
+	return &Logger{
+		level:  level,
+		logger: log.New(os.Stdout, "", 0),
+	}
+}
+
+// Debug logs a debug message
+func (l *Logger) Debug(format string, v ...interface{}) {
+	if l.level <= DEBUG {
+		l.log(DEBUG, format, v...)
+	}
+}
+
+// Info logs an info message
+func (l *Logger) Info(format string, v ...interface{}) {
+	if l.level <= INFO {
+		l.log(INFO, format, v...)
+	}
+}
+
+// Warn logs a warning message
+func (l *Logger) Warn(format string, v ...interface{}) {
+	if l.level <= WARN {
+		l.log(WARN, format, v...)
+	}
+}
+
+// Error logs an error message
+func (l *Logger) Error(format string, v ...interface{}) {
+	if l.level <= ERROR {
+		l.log(ERROR, format, v...)
+	}
+}
+
+// Fatal logs a fatal message and exits
+func (l *Logger) Fatal(format string, v ...interface{}) {
+	if l.level <= FATAL {
+		l.log(FATAL, format, v...)
+		os.Exit(1)
+	}
+}
+
+// log logs a message with the given level
+func (l *Logger) log(level LogLevel, format string, v ...interface{}) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	levelName := levelNames[level]
+	message := fmt.Sprintf(format, v...)
+	l.logger.Printf("[%s] [%s] %s", timestamp, levelName, message)
+}
